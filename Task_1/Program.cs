ae9ca01b285ba6fcc14b1a46c2b81611b62fa735
UzenLe <Uzenkovlesha@mail.ru>
2023-08-12 12:00:31 +0300

    Задача 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number A ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter numer B ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int numA, int numB)
+{
+  int result = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+    return result;
+}
+
+  int exponentiation = Exponentiation(numA, numB);
+  Console.WriteLine("" + exponentiation);
