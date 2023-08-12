d16b9bad55f5b4479b6d4f0e16d6eb264ef77865
UzenLe <Uzenkovlesha@mail.ru>
2023-08-12 13:14:50 +0300

    Задача 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("Enter number A ");
+﻿/*
+Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+      3, 5 -> 243 (3⁵)
+      2, 4 -> 16
+*/
+
+
+
+
+Console.WriteLine("Enter number A ");
 int numA = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter numer B ");
---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+*/
+
+Console.Write("Enter number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int leng = Length(Num);
+
+Console.Write($"Сумма цифр числа {Num} = {Sum(Num,leng)}");
+
+int Length(int Num)
+{
+    int result = 0;
+    while (Num > 0)
+    {
+         Num /= 10;
+         result++;
+    }
+    return result;
+}   
+int Sum(int Num, int leng)
+{
+    int sum = 0;
+    for (int i = 1; i <= leng; i++)
+    {
+      sum += Num % 10;
+      Num /= 10;
+    }
+    return sum;
+}
